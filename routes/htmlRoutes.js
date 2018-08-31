d4beafa0e09bd8230e9fa0f1d70b5cd1faa5c18f
Asteria <dcdion22@gmail.com>
2018-08-31 11:15:10 -0400

    SideNav working && New Account Page && Working on Handlebars.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,6 +11,26 @@ module.exports = function(app) {
     });
   });
 
+  // Load dash page
+  app.get("/feed", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("dash", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+
+  // Load new-user page
+  app.get("/new-user", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("createAcc", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
