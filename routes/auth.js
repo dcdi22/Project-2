e5767957b396b9cec08ffce3ac67f5200f55e1ff
Asteria <dcdion22@gmail.com>
2018-09-06 18:28:10 -0400

    LogOut Working

--- routes/auth.js
+++ routes/auth.js
@@ -10,21 +10,6 @@ module.exports = (app, passport) => {
     });
   });
 
-  // app.get("/logout", (req, res) => {
-  //   req.session.destroy();
-  //   console.log("HELLLLOOOOOOOOOOOOO88888");
-  //   res.redirect("/");
-  // });
-
-  // app.get('/logout', function(req, res){
-  //   req.logout();
-  //   res.redirect('/');
-  // });
-
-  //   app.get("/signin", (req, res) => {
-  //     res.render("signin");
-  //   });
-
   app.post(
     "/signin",
     passport.authenticate("local-signin", {
